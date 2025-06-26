1c2a4eb2c8388b3a78172155994fc5e3fc46233a
JatinSinghRawat <jatinsinghrawat759@gmail.com>
2025-06-26 06:56:29 +0530

    Implemented Authentication and  Subscription Plan using Clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Navitems from "./Navitems"
 
 const Navbar = () => {
@@ -15,8 +16,15 @@ const Navbar = () => {
             </div>
         </Link>
         <div className="flex items-center gap-8">
-            <Navitems></Navitems>
-            <p>Sign In</p>
+            <Navitems/>
+              <SignedOut>
+                <SignInButton>
+                    <button className="btn-signin">Sign In</button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton/>
+              </SignedIn>
         </div>
     </nav>
   )
